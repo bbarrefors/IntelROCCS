fa346bc05a7351aa15318b0d6f7fc68d28124d65
Bjorn Barrefors <barrefors@gmail.com>
2014-06-13 15:59:15 -0500

    Bug fixes

--- APIs/dbAccess.py
+++ APIs/dbAccess.py
@@ -4,7 +4,7 @@
 # dbAccess
 #
 #---------------------------------------------------------------------------------------------------
-import sys, MySQLdb
+import sys, os, base64, MySQLdb
 
 class dbAccess():
     def __init__(self):
@@ -19,18 +19,28 @@ class dbAccess():
 #===================================================================================================
 #  H E L P E R S
 #===================================================================================================
-	def dbQuery(self, query, values):
-		cur = self.DB_CON.cursor()
-		cur.execute(query, values))
+    def dbQuery(self, query, values):
+        data = []
+        try:
+            with self.DB_CON:
+                cur = self.DB_CON.cursor()
+                cur.execute(query, values)
+                for i in range(cur.rowcount):
+                    row = cur.fetchone()
+                    data.append(row)
+        except MySQLdb.Error, e:
+            raise Exception("FATAL - Database failure:  %d: %s" % (e.args[0],e.args[1]))
+        return data
 
 #===================================================================================================
 #  M A I N
 #===================================================================================================
 # Use this for testing purposes or as a script. 
-# Usage: python ./dbAccess.py <query> [value1, value2, ...]
+# Usage: python ./dbAccess.py <'db query'> ['value1', 'value2', ...]
+# Example: $ python ./dbAccess.py 'SELECT * WHERE DatasetName=%s' 'Dataset1'
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print "Usage: python ./dbAccess.py <query> [value1, value2, ...]"
+        print "Usage: python ./dbAccess.py <'db query'> ['value1', 'value2', ...]"
         sys.exit(2)
     mit_db = dbAccess()
     query = sys.argv[1]
