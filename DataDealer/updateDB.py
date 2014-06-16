1f94752356e995da3a7f664a73f17a89383641cf
Bjorn Barrefors <barrefors@gmail.com>
2014-06-16 16:28:00 -0500

    Bug fixes

--- DataDealer/updateDB.py
+++ DataDealer/updateDB.py
@@ -4,7 +4,7 @@
 # DataDealer
 #
 #---------------------------------------------------------------------------------------------------
-import sys
+import sys, os
 BASEDIR = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
 for root, dirs, files in os.walk(BASEDIR):
     sys.path.append(root)
@@ -19,26 +19,33 @@ class updateDB():
 #===================================================================================================
     def updateReplicas(self, dataset, replicas):
         # Change query when tables are updated
-        query = "INSERT INTO Replicas (DatasetId, Replicas) SELECT Datasets.DatasetId, %s INNER JOIN WHERE GroupName=%s"
+        query = "INSERT INTO Replicas (DatasetId, Replicas) SELECT Datasets.DatasetId, %s FROM Datasets WHERE DatasetName=%s"
         values = [replicas, dataset]
-        data = dbAcc.dbQuery(query, values=tuple(values))
-        return [site[0] for site in data]
+        data = self.dbAcc.dbQuery(query, values=tuple(values))
 
 #===================================================================================================
 #  M A I N
 #===================================================================================================
 # Use this for testing purposes or as a script. 
-# Usage: python ./updateDB.py <function>
+# Usage: python ./updateDB.py <function> <dataset> <replicas>
 if __name__ == '__main__':
-    if not (len(sys.argv) == 2):
-        print "Usage: python ./updateDB.py <function>"
+    if not (len(sys.argv) == 4):
+        print "Usage: python ./updateDB.py <function> <dataset> <replicas>"
         sys.exit(2)
     dbUpdate = updateDB()
     func = getattr(dbUpdate, sys.argv[1], None)
     if not func:
         print "Function %s is not available" % (sys.argv[1])
-        print "Usage: python ./updateDB.py <function>"
+        print "Usage: python ./updateDB.py <function> <dataset> <replicas>"
         sys.exit(3)
-    data = func()
+    args = []
+    for arg in sys.argv[2:]:
+        args.append(arg)
+    try:
+        data = func(*args)
+    except TypeError, e:
+        print e
+        print "Usage: python ./updateDB.py <function> <dataset> <replicas>"
+        sys.exit(3)
     print data
     sys.exit(0)
